381303d62699b41a6a6d9dca12f4877dc7f356ea
Naman Bhatia <bhatia.naman@hotmail.com>
2021-05-22 00:11:18 +0530

    retrieving data from realtor model in views

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -12,7 +13,16 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtor = Realtor.objects.order_by('-hire_date')
+
+    # Get mvp
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtor,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
 
 
 
