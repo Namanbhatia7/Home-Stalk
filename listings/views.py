9afc72cb606d9413408c1ca03ba16a2846845d2d
Naman Bhatia <bhatia.naman@hotmail.com>
2022-02-14 19:07:22 +0530

    added: search via city functionality

--- listings/views.py
+++ listings/views.py
@@ -32,6 +32,11 @@ def search(request):
         keywords = request.GET['keywords']
         if keywords:
             queryset_list = queryset_list.filter(description__icontains=keywords)
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
 
 
     context = {
